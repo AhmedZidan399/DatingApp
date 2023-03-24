8a1d12754fb6a05e340574acae35ea0404a48ff1
AhmedZidan399 <ahmed.zidan.new10@gmail.com>
2023-03-25 01:17:15 +0200

    upload initial files for DatingApp

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users
+
+    public class UsersController: ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet(Name = "Users")]
+        public async Task<ActionResult<IEnumerable<UsersApp>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsersApp>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
